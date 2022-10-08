1f559f344d6e49eed9ae984977b10ae80e0ea647
maxChen <kingcall550102@gmail.com>
2022-10-08 19:32:08 +0800

    修改資料庫

--- Back_end_internSystem/django_api_test/settings.py
+++ Back_end_internSystem/django_api_test/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mydb',
         'USER': 'root',
-        'PASSWORD': 'aA29882702',
+        'PASSWORD': '你的密碼',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
